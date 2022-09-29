5d6fe320f2a83aaaa7985c8103dfb1f79f33ff97
Sergejus Bykovas <sergejus83@gmail.com>
2022-09-29 14:11:50 +0300

    pradetas projektas

--- password_holder.py
+++ password_holder.py
@@ -2,3 +2,20 @@
 # aprasyti funkcijas
 # leisti vartuotojui ivesti duomenys
 # issaugoti duomenys i faila
+
+from tkinter import *
+langas = Tk()
+langas.geometry("400x200")
+
+
+l_website = Label(langas, text="Website:")
+e_website = Entry(langas)
+l_acount = Label(langas, text="Acount:")
+e_acount = Entry(langas)
+l_password = Label(langas, text="Password:")
+
+
+l_website.grid(row=0,column=0)
+e_website.grid()
+
+langas.mainloop()
